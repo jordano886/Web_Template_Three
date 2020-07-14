5ce11c8b39544ecfc4881e425bf6a1005189c896
Jordano <jordano886@hotmail.com>
2020-07-14 11:51:13 -0400

    Plantilla Web 3

---
+++ main.js
@@ -0,0 +1,11 @@
+/** Efecto de guardar el boton de menu en movil */
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+/** Efecto */
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
